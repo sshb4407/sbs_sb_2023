d05180f22812707b4fda100ea68e604cc67f3dc7
sshb4407 <sshb4407@naver.com>
2023-08-10 12:21:18 +0900

    처 음

---
+++ src/main/java/com/sdw/exam/demo/controller/userhomeController.java
@@ -0,0 +1,29 @@
+package com.sdw.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class userhomeController {
+	@RequestMapping("/user/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+	@RequestMapping("/user/home/main2")
+	@ResponseBody
+		public String showMain2() {
+			return "반갑습니다.";
+	}
+	@RequestMapping("/user/home/main3")
+	@ResponseBody
+		public String showMain3() {
+			return "또만나요";
+	}
+	@RequestMapping("/user/home/main4")
+	@ResponseBody
+		public String showMain4() {
+			return "또만나요";
+	}
+}
