7807cb3e057fc8c3ada28e9c489f7a00432d6c09
sshb4407 <sshb4407@naver.com>
2023-08-10 13:45:34 +0900

    테스트 게시물 10개 자동생성

--- src/main/java/com/sdw/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/sdw/exam/demo/controller/UserArticleController.java
@@ -17,8 +17,23 @@ public class UserArticleController {
 	public UserArticleController() {
 		articleLastId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
+	private void makeTestData() {
+		for ( int i = 1; i <= 10; i++ ) {
+			int id = articleLastId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			articleLastId = id;
+		}
+	}
+
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
