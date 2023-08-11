65223cea5f0be68ac968c3dc111eb103963e7714
sshb4407 <sshb4407@naver.com>
2023-08-11 12:01:26 +0900

    ArticleService에 있던 단순데이터 저장과 관련된 로직은 ArticleRepository로 옮김

---
+++ src/main/java/com/sdw/exam/demo/repository/ArticleRepository.java
@@ -0,0 +1,66 @@
+package com.sdw.exam.demo.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sdw.exam.demo.vo.Article;
+
+@Component
+public class ArticleRepository {
+	private int articleLastId;
+	private List<Article> articles;
+	
+	public ArticleRepository() {
+		articleLastId = 0;
+		articles = new ArrayList<>();
+	}
+	
+	public void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+		int id = articleLastId + 1;
+
+		Article article = new Article(id, title, body);
+
+		articles.add(article);
+		articleLastId = id;
+
+		return article;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		return null;
+	}
+
+	public void deleteArticle(int id) {
+		Article article = getArticle(id);
+
+		articles.remove(article);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
+}
--- src/main/java/com/sdw/exam/demo/service/ArticleService.java
+++ src/main/java/com/sdw/exam/demo/service/ArticleService.java
@@ -1,72 +1,42 @@
 package com.sdw.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.sdw.exam.demo.repository.ArticleRepository;
 import com.sdw.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	// 인스턴스 변수 시작
-	private int articleLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
+	ArticleRepository articleRepository;
 	
-	public ArticleService() {
-		articleLastId = 0;
-		articles = new ArrayList<>();
-
-		makeTestData();
-	}
-
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		
+		articleRepository.makeTestData();
 	}
 
 	public Article writeArticle(String title, String body) {
-		int id = articleLastId + 1;
-
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articleLastId = id;
-
-		return article;
+		return articleRepository.writeArticle(title, body);
 	}
 	
 	public List<Article> getArticles() {
-		return articles;
+		return articleRepository.getArticles();
 	}
 
 	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-
-		return null;
+		return articleRepository.getArticle(id);
 	}
 
 	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
+		articleRepository.deleteArticle(id);
 
 	}
 
 	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
+		articleRepository.modifyArticle(id, title, body);
 
 	}
 }
