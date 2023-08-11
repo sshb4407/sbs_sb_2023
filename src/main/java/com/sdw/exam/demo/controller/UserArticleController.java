e0f40ed87abcb2896f9e4097f17b7abd6e397fd8
sshb4407 <sshb4407@naver.com>
2023-08-11 09:08:42 +0900

    매서드를 통해 중복로직 제거

--- src/main/java/com/sdw/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/sdw/exam/demo/controller/UserArticleController.java
@@ -23,20 +23,14 @@ public class UserArticleController {
 	
 	private void makeTestData() {
 		for ( int i = 1; i <= 10; i++ ) {
-			int id = articleLastId + 1;
 			String title = "제목" + i;
 			String body = "내용" + i;
 			
-			Article article = new Article(id, title, body);
-			
-			articles.add(article);
-			articleLastId = id;
+			writeArticle(title, body);
 		}
 	}
-
-	@RequestMapping("/user/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	
+	public Article writeArticle(String title, String body) {
 		int id = articleLastId + 1;
 		
 		Article article = new Article(id, title, body);
@@ -46,8 +40,15 @@ public class UserArticleController {
 		
 		return article;
 	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+		return article;
+	}
 	
-	@RequestMapping("/user/article/getArticles")
+	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
