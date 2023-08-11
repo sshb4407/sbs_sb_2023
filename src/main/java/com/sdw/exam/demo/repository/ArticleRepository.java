59fa147aad335a6e9b4085da378f749cf688fbeb
sshb4407 <sshb4407@naver.com>
2023-08-11 13:25:56 +0900

    INSERT 쿼리를 메서드와 연결,last_insert_id 사용

--- src/main/java/com/sdw/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/sdw/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.sdw.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -12,22 +13,21 @@ import com.sdw.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	// INSERT INTO aritcle SET regDate = NOW(), updateDate = NOW(), title = ?, `body`= ?;
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body`= #{body}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-	// SELECT * FROM article ORDER BY id DESC;
 	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 
-	// SELECT * FROM article WHERE id = ?
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	// DELETE * FROM article WHERE id = ?
 	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
 	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(@Param("id")int id, @Param("title") String title, @Param("body") String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 }
