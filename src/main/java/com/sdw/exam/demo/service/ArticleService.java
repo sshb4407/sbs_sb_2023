b8e09963dea23fcc2192f7e64b7173d4ab1eecf2
sshb4407 <sshb4407@naver.com>
2023-08-16 11:16:47 +0900

    usr/article/getArticles 및 doAdd에 ResultData 적용

--- src/main/java/com/sdw/exam/demo/service/ArticleService.java
+++ src/main/java/com/sdw/exam/demo/service/ArticleService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.sdw.exam.demo.repository.ArticleRepository;
+import com.sdw.exam.demo.util.Ut;
 import com.sdw.exam.demo.vo.Article;
+import com.sdw.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -15,10 +17,11 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
+		int id = articleRepository.getLastInsertId();
 		
-		return articleRepository.getLastInsertId();
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 	
 	public List<Article> getArticles() {
--- src/main/java/com/sdw/exam/demo/vo/ResultData.java
+++ src/main/java/com/sdw/exam/demo/vo/ResultData.java
@@ -9,25 +9,30 @@ public class ResultData {
 	private String msg;
 	@Getter
 	private Object data1;
-	
+
 	private ResultData() {
-		
+
 	}
-	
+
 	public static ResultData from(String reslutCode, String msg, Object data1) {
 		ResultData rd = new ResultData();
 		rd.resultCode = reslutCode;
 		rd.msg = msg;
 		rd.data1 = data1;
-		
-		return rd;				
+
+		return rd;
 	}
-	
+
+	public static ResultData from(String resultCode, String msg) {
+		return from(resultCode, msg, null);
+	}
+
 	public boolean isSuccess() {
 		return resultCode.startsWith("S-");
 	}
-	
+
 	public boolean isFail() {
 		return isSuccess() == false;
 	}
+
 }
