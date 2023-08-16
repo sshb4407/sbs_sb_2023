1a453d189c597f09c8e6f3c9912e4a79dbc71a6f
sshb4407 <sshb4407@naver.com>
2023-08-16 10:23:33 +0900

    범용 보고서 클래스,ResultData 구현

---
+++ src/main/java/com/sdw/exam/demo/vo/ResultData.java
@@ -0,0 +1,33 @@
+package com.sdw.exam.demo.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data1;
+	
+	private ResultData() {
+		
+	}
+	
+	public static ResultData from(String reslutCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+		rd.resultCode = reslutCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		
+		return rd;				
+	}
+	
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	
+	public boolean isFail() {
+		return isSuccess() == false;
+	}
+}
